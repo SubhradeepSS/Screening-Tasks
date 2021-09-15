a45626d0079daa4f33829ca70b917286e325ef36
Subhradeep Saha <subhradeep_saha@outlook.com>
2021-09-15 14:49:39 +0530

    👷CI: added ci for ContactBook project

--- ContactBook/api/core/views.py
+++ ContactBook/api/core/views.py
@@ -1,26 +1,22 @@
 from .serializers import ContactSerializer
 from .models import Contact
-from django.http import JsonResponse
-from rest_framework import generics, permissions
+from rest_framework import generics
 
 
 # Create your views here.
 class ContactsView(generics.ListCreateAPIView):
     queryset = Contact.objects.all()
     serializer_class = ContactSerializer
-    permission_classes = [permissions.IsAuthenticated]
 
 
 class ContactView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Contact.objects.all()
     serializer_class = ContactSerializer
-    permission_classes = [permissions.IsAuthenticated]
 
 
 class SearchNameView(generics.ListAPIView):
     lookup_url_kwarg = 'name'
     serializer_class = ContactSerializer
-    permission_classes = [permissions.IsAuthenticated]
 
     def get_queryset(self):
         return Contact.objects.filter(name__contains=self.kwargs.get(self.lookup_url_kwarg))
@@ -29,7 +25,6 @@ class SearchNameView(generics.ListAPIView):
 class SearchEmailView(generics.ListAPIView):
     lookup_url_kwarg = 'email'
     serializer_class = ContactSerializer
-    permission_classes = [permissions.IsAuthenticated]
 
     def get_queryset(self):
         return Contact.objects.filter(email=self.kwargs.get(self.lookup_url_kwarg))
